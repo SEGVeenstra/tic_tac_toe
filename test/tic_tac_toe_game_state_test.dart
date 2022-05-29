5831ca862522e813ec74a44ac733ab101ab93266
SEGVeenstra <seg.veenstra@gmail.com>
2022-05-29 20:20:28 +0200

    Add test to prevent alteration

--- test/tic_tac_toe_game_state_test.dart
+++ test/tic_tac_toe_game_state_test.dart
@@ -7,12 +7,29 @@ void main() {
     'constructor',
     () {
       test('should return initial state', () {
-        final TicTacToeGameState state = TicTacToeGameState();
+        final state = TicTacToeGameState();
 
         expect(state.fields,
             equals([null, null, null, null, null, null, null, null, null]));
         expect(state.status, Status.p1Turn);
       });
+    },
+  );
+
+  group(
+    'fields',
+    () {
+      test(
+        'updating retrieved fields should not update internal state',
+        () {
+          final state = TicTacToeGameState();
+
+          state.fields[0] = Player.p1;
+
+          expect(state.fields,
+              [null, null, null, null, null, null, null, null, null]);
+        },
+      );
     },
   );
 }
