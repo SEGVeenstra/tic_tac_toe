c341899505799edf9a7911f8b9c485f202eff1e4
SEGVeenstra <seg.veenstra@gmail.com>
2022-06-05 21:12:22 +0200

    Add win states tests

--- lib/src/tic_tac_toe_game_state.dart
+++ lib/src/tic_tac_toe_game_state.dart
@@ -1,3 +1,5 @@
+import 'package:meta/meta.dart';
+
 /// The Players for the Tic Tac Toe game.
 enum Player { p1, p2 }
 
@@ -6,10 +8,42 @@ enum Status { p1Turn, p2Turn, p1Won, p2Won, draw }
 
 /// The state of a Tic Tac Toe Game
 class TicTacToeGameState {
+  final List<Player?> _fields;
+
   /// Gets the [fields] with the info about which [Player] claimed it.
-  List<Player?> get fields =>
-      [null, null, null, null, null, null, null, null, null];
+  List<Player?> get fields => List.from(_fields);
 
   /// Returns the [Status] of the game
-  Status get status => Status.p1Turn;
+  Status get status {
+    if (_hasThreeInARow(Player.p1)) return Status.p1Won;
+    if (_hasThreeInARow(Player.p2)) return Status.p2Won;
+
+    final occupiedFields = _fields.where((field) => field != null);
+
+    if (occupiedFields.length == 9) return Status.draw;
+
+    final hasEvenAmountOfOccupiedFields = occupiedFields.length % 2 == 0;
+
+    return hasEvenAmountOfOccupiedFields ? Status.p1Turn : Status.p2Turn;
+  }
+
+  TicTacToeGameState()
+      : _fields = [null, null, null, null, null, null, null, null, null];
+
+  @visibleForTesting
+  TicTacToeGameState.seed(this._fields);
+
+  bool _hasThreeInARow(Player player) {
+    if (_fields[0] == player && _fields[1] == player && _fields[2] == player ||
+        _fields[3] == player && _fields[4] == player && _fields[5] == player ||
+        _fields[6] == player && _fields[7] == player && _fields[8] == player ||
+        _fields[0] == player && _fields[3] == player && _fields[6] == player ||
+        _fields[1] == player && _fields[4] == player && _fields[7] == player ||
+        _fields[2] == player && _fields[5] == player && _fields[8] == player ||
+        _fields[0] == player && _fields[4] == player && _fields[8] == player ||
+        _fields[2] == player && _fields[4] == player && _fields[6] == player) {
+      return true;
+    }
+    return false;
+  }
 }
--- test/tic_tac_toe_game_state_test.dart
+++ test/tic_tac_toe_game_state_test.dart
@@ -32,4 +32,60 @@ void main() {
       );
     },
   );
+
+  group(
+    'status',
+    () {
+      final testCases = <String, Status>{
+        // Turns
+        'OO-|XOX|-X-': Status.p1Turn,
+        'X-X|---|--O': Status.p2Turn,
+        // Draw
+        'XOX|XOX|OXO': Status.draw,
+        // Win states player 1
+        'XXX|OO-|---': Status.p1Won,
+        'X-O|XXX|O-O': Status.p1Won,
+        '---|O-O|XXX': Status.p1Won,
+        'X--|X-O|X-O': Status.p1Won,
+        '-X-|-XO|-XO': Status.p1Won,
+        '--X|-OX|-OX': Status.p1Won,
+        'X-O|-XO|--X': Status.p1Won,
+        'O-X|-XO|X--': Status.p1Won,
+        // Win states player 2
+        'OOO|XX-|-X-': Status.p2Won,
+        'X-X|OOO|-X-': Status.p2Won,
+        'X-X|--X|OOO': Status.p2Won,
+        'O--|O-X|OXX': Status.p2Won,
+        '-O-|-OX|XOX': Status.p2Won,
+        '--O|-XO|XXO': Status.p2Won,
+        'O-X|-O-|XXO': Status.p2Won,
+        'X-O|-O-|OXX': Status.p2Won,
+      };
+
+      testCases.forEach(
+        (state, status) => test(
+          '$state should result in $status',
+          () {
+            expect(state.asTicTacToeGameState.status, status);
+          },
+        ),
+      );
+    },
+  );
+}
+
+extension on String {
+  TicTacToeGameState get asTicTacToeGameState {
+    final fields = replaceAll('|', '').split('').map((char) {
+      switch (char) {
+        case 'X':
+          return Player.p1;
+        case 'O':
+          return Player.p2;
+        default:
+          null;
+      }
+    }).toList();
+    return TicTacToeGameState.seed(fields);
+  }
 }
